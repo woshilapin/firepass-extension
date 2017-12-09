0b6a1e5ae25d4fcdd3ab8ff123ca9aa8d41ff373
Jean Simard <jean.simard@dominos.com>
2017-12-08 22:13:57 -0500

    Send the correct payload

--- src/firepass.js
+++ src/firepass.js
@@ -1,17 +1,9 @@
-let counter = 0;
 browser.browserAction.onClicked.addListener(async () => {
-	let message = {
-		count: counter,
-		content: 'ping'
-	};
-	console.log('SENDING');
-	console.log(message);
-	counter++;
+	let args = ['perso/web/test'];
 	try {
-		let response = await browser.runtime.sendNativeMessage('firepass', message);
-		console.log('RECEIVED');
+		let response = await browser.runtime.sendNativeMessage('firepass', args);
 		console.log(response);
 	} catch(error) {
-		console.error('Error: ' + error);
+		console.error(error);
 	}
 });
