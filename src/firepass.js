6744d65f8d832cb6696dced66c611bab3d179a98
Jean Simard <jean.simard@dominos.com>
2017-12-08 22:41:16 -0500

    Send more complexe commands

--- src/firepass.js
+++ src/firepass.js
@@ -1,7 +1,8 @@
 browser.browserAction.onClicked.addListener(async () => {
+	let command = 'show';
 	let args = ['perso/web/test'];
 	try {
-		let response = await browser.runtime.sendNativeMessage('firepass', args);
+		let response = await browser.runtime.sendNativeMessage('firepass', {command, args});
 		console.log(response);
 	} catch(error) {
 		console.error(error);
