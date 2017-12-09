19c9241b5af7577be3f97bb04df3e64a1420a848
Jean Simard <jean.simard@dominos.com>
2017-12-08 22:49:03 -0500

    Improve support of 'show' operation

--- src/firepass.js
+++ src/firepass.js
@@ -1,6 +1,8 @@
 browser.browserAction.onClicked.addListener(async () => {
 	let command = 'show';
-	let args = ['perso/web/test'];
+	let args = {
+		"path": 'perso/web/test'
+	};
 	try {
 		let response = await browser.runtime.sendNativeMessage('firepass', {command, args});
 		console.log(response);
