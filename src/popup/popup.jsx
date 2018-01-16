e67aaef4a146e9ce0328b4939e2ef133640c4cf2
Jean Simard <jean.simard@dominos.com>
2018-01-15 22:07:35 -0500

    Add localStorage

--- src/popup/popup.jsx
+++ src/popup/popup.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-let command = 'ls';
-let args = {
-	"path": 'perso/web/test'
+let storage = window.localStorage;
+let render = (firepass) => {
+	let app = <App logins={firepass.logins} />;
+	ReactDOM.render(app, document.getElementById('app'));
 };
+if(storage.getItem('firepass')) {
+	render(JSON.parse(storage.getItem('firepass')));
+}
 window.addEventListener('load', async () => {
 	try {
-		let logins = await browser.runtime.sendNativeMessage('firepass', {command, args})
-		let app = <App logins={logins} />;
-		ReactDOM.render(app, document.getElementById('app'));
+		let logins = await browser.runtime.sendNativeMessage('firepass', {
+			"command": 'ls',
+			"args": {}
+		});
+		let firepass = Object.assign({}, JSON.parse(storage.getItem('firepass')), {logins});
+		storage.setItem('firepass', JSON.stringify(firepass));
+		render(firepass);
 	} catch(error) {
 		console.error(error);
 	}
